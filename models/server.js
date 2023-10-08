f88b5250dcb8bae2df2fab977c3eb784eb70036e
AG-alexander <46871080+AG-alexander@users.noreply.github.com>
2023-10-08 15:54:08 -0600

    Update server.js
    Corrección puerto por defecto

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT | 8080;
+        this.port = process.env.PORT || 8080;
         this.userPath = '/api/user';
 
         //  Middlewares
